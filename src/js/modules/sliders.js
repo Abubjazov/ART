58f07f6ac6cd670aadb045d010e3111c3496cf07
Radik Abubjazov <abubjazov@mail.ru>
2021-07-22 21:30:21 +0300

    доработан скрипт sliders

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -1,5 +1,7 @@
 const sliders = (slides, direction, prev, next) => {
-    let slideIndex = 1;
+    let slideIndex = 1,
+        paused = false;
+
     const items = document.querySelectorAll(slides);
 
     function showSlides(n) {
@@ -43,18 +45,30 @@ const sliders = (slides, direction, prev, next) => {
 
     } catch(e) {}
 
-    if (direction === 'vertical') {
-        setInterval(function (){
-            changeSlide(1);
-            items[slideIndex - 1].classList.add('slideInDown');
-        }, 5000);
-    } else {
-        setInterval(function (){
-            changeSlide(1);
-            items[slideIndex - 1].classList.remove('slideInRight');
-            items[slideIndex - 1].classList.add('slideInLeft');
-        }, 5000);
+    function activateAnimation () {
+        if (direction === 'vertical') {
+            paused = setInterval(function () {
+                changeSlide(1);
+                items[slideIndex - 1].classList.add('slideInDown');
+            }, 4000);
+        } else {
+            paused = setInterval(function () {
+                changeSlide(1);
+                items[slideIndex - 1].classList.remove('slideInRight');
+                items[slideIndex - 1].classList.add('slideInLeft');
+            }, 4000);
+        }
     }
+    activateAnimation();
+    
+    items[0].parentNode.addEventListener('mouseenter', () => {
+        clearInterval(paused);
+    });
+    items[0].parentNode.addEventListener('mouseleave', () => {
+        activateAnimation();
+    });
+
+    
 };
 
 export default sliders;
