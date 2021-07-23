5c7ba1b10591b36be551454c14dda8a24bc06b34
Radik Abubjazov <abubjazov@mail.ru>
2021-07-23 15:28:33 +0300

    скрипт showMoreStyles: рефакторинг

--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -12,13 +12,13 @@ const showMoreStyles = (trigger, wrapper) => {
     });
 
     function createCards(response) {
-        response.forEach(huyatem => {
+        response.forEach(({src, title, link}) => {
             let card = document.createElement('div');
             card.classList.add('animated', 'fadeInUp', 'col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
             card.innerHTML = `<div class="styles-block">
-                                <img src=${huyatem.src} alt="style">
-                                <h4>${huyatem.title}</h4>
-                                <a href="${huyatem.link}">Подробнее</a>
+                                <img src=${src} alt="style">
+                                <h4>${title}</h4>
+                                <a href="${link}">Подробнее</a>
                               </div>
             `;
 
